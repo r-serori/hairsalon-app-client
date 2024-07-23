8c580081c4acc8497fce80f0eca38c783df26938
r-serori <ryunosuke.sakaue@gmail.com>
2024-07-23 14:31:33 +0900

    テスト中

--- app/components/elements/form/customers/CustomerForm.tsx
+++ app/components/elements/form/customers/CustomerForm.tsx
@@ -51,30 +51,28 @@ const CustomerForm: React.FC<CustomerFormProps> = ({
     node === null || node.course_id === null
       ? []
       : getCoursesState
-          .filter((course) => node?.course_id.includes(course.id))
+          .filter((course) => node.course_id.includes(course.id))
           .map((course) => course.course_name)
   );
   const [options, setOptions] = useState<string[]>(
     node === null || node.option_id === null
       ? []
       : getOptionsState
-          .filter((option) => node?.option_id.includes(option.id))
+          .filter((option) => node.option_id.includes(option.id))
           .map((option) => option.option_name)
   );
   const [merchandises, setMerchandises] = useState<string[]>(
     node === null || node.merchandise_id === null
       ? []
       : getMerchandisesState
-          .filter((merchandise) =>
-            node?.merchandise_id.includes(merchandise.id)
-          )
+          .filter((merchandise) => node.merchandise_id.includes(merchandise.id))
           .map((merchandise) => merchandise.merchandise_name)
   );
   const [hairstyles, setHairstyles] = useState<string[]>(
     node === null || node.hairstyle_id === null
       ? []
       : getHairstylesState
-          .filter((hairstyle) => node?.hairstyle_id.includes(hairstyle.id))
+          .filter((hairstyle) => node.hairstyle_id.includes(hairstyle.id))
           .map((hairstyle) => hairstyle.hairstyle_name)
   );
   const [users, setUsers] = useState<string[]>(
