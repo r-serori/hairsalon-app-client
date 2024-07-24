b33bf36802585fd194295ac3aebf33f2c9aeb2e8
r-serori <ryunosuke.sakaue@gmail.com>
2024-07-25 02:52:52 +0900

    テスト中

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,6 +1,6 @@
 // _app.tsx
 
-import React, { useEffect } from "react";
+import React from "react";
 import { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import store from "../redux/store"; // Assuming your store file is named index.ts or store.ts
