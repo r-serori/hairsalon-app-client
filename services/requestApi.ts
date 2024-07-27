36b087c9473c433d18488d1f0f3749f7d87128c8
r-serori <ryunosuke.sakaue@gmail.com>
2024-07-27 14:17:05 +0900

    requestApi.tsを書き換え

--- services/requestApi.ts
+++ services/requestApi.ts
@@ -10,13 +10,13 @@ export const getCsrfToken = async () => {
   try {
     await axios.get("/sanctum/csrf-cookie");
     // CSRFトークンを取得
-    const csrfToken = document.cookie.replace(
-      /(?:(?:^|.*;\s*)XSRF-TOKEN\s*=\s*([^;]*).*$)|^.*$/,
-      "$1"
-    );
-    console.log("CSRFトークン取得成功:", csrfToken);
-    axios.defaults.headers.common["X-XSRF-TOKEN"] = csrfToken;
-    return csrfToken;
+    // const csrfToken = document.cookie.replace(
+    //   /(?:(?:^|.*;\s*)XSRF-TOKEN\s*=\s*([^;]*).*$)|^.*$/,
+    //   "$1"
+    // );
+    // console.log("CSRFトークン取得成功:", csrfToken);
+    // axios.defaults.headers.common["X-XSRF-TOKEN"] = csrfToken;
+    // return csrfToken;
   } catch (error) {
     // console.error("CSRFトークンの取得に失敗しました:", error);
     throw new Error("CSRFトークンの取得に失敗しました");
@@ -34,13 +34,13 @@ export const sendRequest = async (
       method,
       url,
       data,
-      withXSRFToken: true, // 追記
-      xsrfHeaderName: "X-XSRF-TOKEN", // 追記
+      // xsrfHeaderName: "X-XSRF-TOKEN", // 追記
       withCredentials: true,
       headers: {
         "Content-Type": "application/json; charset=utf-8",
+
         // CSRF トークンが必要な場合に備えて追加します
-        "X-XSRF-TOKEN": axios.defaults.headers.common["X-XSRF-TOKEN"],
+        // "X-XSRF-TOKEN": axios.defaults.headers.common["X-XSRF-TOKEN"],
       },
     });
 
